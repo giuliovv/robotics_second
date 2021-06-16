07eaff02e4fd9a47d8917d555c6ae9ffaefc61f8
Giulio Vaccari <giulio09@marcov.it>
2021-06-16 22:40:18 +0200

    Inizio tf

---
+++ src/tf_publisher.cpp
@@ -0,0 +1,43 @@
+#include "ros/ros.h"
+#include <geometry_msgs/TwistStamped.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <nav_msgs/Odometry.h>
+
+class tf_sub_pub
+{
+public:
+  tf_sub_pub() {
+    sub = n.subscribe("/odom", 1000, &tf_sub_pub::callback, this);
+  }
+
+  void callback(const nav_msgs::OdometryConstPtr& msg){
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "world";
+    transformStamped.child_frame_id = "base_link";
+    transformStamped.transform.translation.x = msg->pose.pose.position.x;
+    transformStamped.transform.translation.y = msg->pose.pose.position.y;
+    transformStamped.transform.translation.z = msg->pose.pose.position.z;
+
+    transformStamped.transform.rotation.x = msg->pose.pose.orientation.x;
+    transformStamped.transform.rotation.y = msg->pose.pose.orientation.y;
+    transformStamped.transform.rotation.z = msg->pose.pose.orientation.z;
+    transformStamped.transform.rotation.w = msg->pose.pose.orientation.w;
+    br.sendTransform(transformStamped);
+  }
+
+private:
+  ros::NodeHandle n; 
+  tf2_ros::TransformBroadcaster br;
+  geometry_msgs::TransformStamped transformStamped;
+  ros::Subscriber sub;
+};
+
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "tf_odom");
+  tf_sub_pub my_tf_sub_bub;
+  ros::spin();
+  return 0;
+}
